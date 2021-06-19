1049a5abd59177a2892693b8c7e0f6caac260e37
eightgran <eightgran@gmail.com>
2021-06-19 19:18:41 +0200

    Add GlobalStyles and Footer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 /**
- * Implement Gatsby's Browser APIs in this file.
+ * Gatsby's Browser APIs.
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
-
-// You can delete this file if you're not using it
+// Montserrat font package
+import "@fontsource/montserrat"
